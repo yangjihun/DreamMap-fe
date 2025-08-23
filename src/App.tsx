32d03efd84af24e637edaac1231997ecdc48719a
gs07103 <gwonseok02@email.com>
2025-08-24 01:56:40 +0900

    feat: 보호된 경로에 PrivateRoute 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import PrivateRoute from "./components/PrivateRoute";
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
@@ -13,13 +14,19 @@ function App() {
     <Provider store={store}>
       <Router>
         <div className="App">
-          <Routes>
+        <Routes>
+            {/* 로그인이 필요 없는 공용 경로*/}
             <Route path="/" element={<HomePage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/upload" element={<UploadPage />} />
-            <Route path="/analysis/:id" element={<AnalysisPage />} />
-            <Route path="/roadmap/:id" element={<RoadmapPage />} />
+            {/* 로그인이 필요한 보호된 경로                       */}
+            {/* PrivateRoute가 이 경로들에 대한 접근을 제어*/}
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/upload" element={<UploadPage />} />
+              {/* ':id'와 같은 동적 경로도 보호할 수 있다. */}
+              <Route path="/analysis/:id" element={<AnalysisPage />} />
+              <Route path="/roadmap/:id" element={<RoadmapPage />} />
+            </Route>
           </Routes>
         </div>
       </Router>
